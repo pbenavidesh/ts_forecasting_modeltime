95f212cb7c1b204dfc96af3250b37495b3f14f08
pbenavidesh <pbenavides@iteso.mx>
2020-09-15 20:18:38 -0500

    01_jumpstart

---
+++ 01_jumpstart/00_data_overview.R
@@ -0,0 +1,31 @@
+# BUSINESS SCIENCE UNIVERSITY
+# DS4B 203-R: ADVANCED TIME SERIES FORECASTING FOR BUSINESS
+# MODULE: INSPECT COURSE DATASETS
+
+
+# LIBRARIES ----
+library(tidyverse)
+
+
+# DATA -----
+
+# * Establish Relationships ----
+#   - Website traffic (Page Views, Sessions, Organic Traffic)
+#   - Top 20 Pages
+
+read_rds("00_data/google_analytics_summary_hourly.rds")
+
+
+read_rds("00_data/google_analytics_by_page_daily.rds")
+
+# * Build Relationships ----
+#   - Collect emails
+#   - Host Events
+
+
+
+# * Generate Course Revenue ----
+#   - Revenue data (aggregated at weekly interval)
+#   - Product Events
+
+
